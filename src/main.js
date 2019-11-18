d5d533522708138ac5056da28ec7a4873ce2ed0f
yzsn123 <2334272153@qq.com>
2019-11-18 20:21:09 +0800

    main.js

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import plugin from './plugin'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import 'lib-flexible/flexible'
+
+Vue.$center = Vue.prototype.$center = new Vue();
+
+Vue.config.productionTip = false
+
+
+Vue.use(plugin);
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
